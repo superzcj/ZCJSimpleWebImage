c1b8b1b1e648fdb8ea33ab0a7e8394ed77e71cc0
zcj <405911774@qq.com>
2016-10-14 13:44:22 +0800

    Initial Commit

---
+++ ZCJSimpleWebImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZCJSimpleWebImage
+//
+//  Created by zhangchaojie on 16/10/14.
+//  Copyright © 2016年 zhangchaojie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZCJSimpleWebImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZCJSimpleWebImage
+//
+//  Created by zhangchaojie on 16/10/14.
+//  Copyright © 2016年 zhangchaojie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
